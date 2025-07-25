53d3baa3f800b602e90167223e31e5c4ca922520
TOMBOY <bohyemian.kr@gmail.com>
2025-07-25 16:12:28 +0900

    children type 추가

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const layout = ({ children }) => {
+const layout = ({ children }: { children: React.ReactNode }) => {
   return <div className="flex justify-center items-center absolute inset-0">{children}</div>;
 };
 
