8c12a35784019582a388ba38f750c9797c783a9c
TOMBOY <bohyemian.kr@gmail.com>
2025-07-26 11:32:46 +0900

    디렉토리 추가

---
+++ app/explore/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const page = (props) => {
+  return (
+    <div>{props.searchParams.list}</div>
+  )
+}
+
+export default page
--- components/ErrorMessage.tsx
+++ components/ErrorMessage.tsx
@@ -2,10 +2,10 @@ import { FcFlashOn } from "react-icons/fc"
 
 const ErrorMessage = () => {
   return (
-    <div className="flex flex-col justify-center items-center gap-2 absolute inset-0">
+    <div className="flex flex-col justify-center items-center gap-1 absolute inset-0">
       <FcFlashOn size="48" />
-      <strong className="text-3xl">Error!</strong>
-      <p className="py-8">잠시 후 다시 확인해 주세요. 🤨</p>
+      <strong className="text-4xl">Error!</strong>
+      <p className="py-4">잠시 후 다시 확인해 주세요. 🤨</p>
     </div>
   )
 }
