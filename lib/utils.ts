07752c5fe533a2ce0649e9e8ebfcbe98ba79053d
TOMBOY <bohyemian.kr@gmail.com>
2025-08-04 18:39:35 +0900

    getRandomElementFromArray util 추가

--- lib/utils.ts
+++ lib/utils.ts
@@ -14,3 +14,7 @@ export const getRandomInt = (min: number, max: number) => {
 
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+export function getRandomElementFromArray(arr: any[]) {
+  return arr[getRandomInt(0, arr.length - 1)];
+}
