7b4ad11fdea78301a9f2685b148f8e8e74937a63
TOMBOY <bohyemian.kr@gmail.com>
2025-08-07 12:38:52 +0900

    공통 레이아웃 적용

--- components/elements/IconButton.tsx
+++ components/elements/IconButton.tsx
@@ -1,7 +1,7 @@
 import { ReactNode } from 'react';
 
-const IconButton = ({ icon, onIconClick }: { icon: ReactNode; onIconClick?: () => void }) => {
-  return <div onClick={onIconClick}>{icon}</div>
+const IconButton = ({ icon, className, onIconClick }: { icon: ReactNode; className?: string; onIconClick?: () => void }) => {
+  return <div className={className} onClick={onIconClick}>{icon}</div>
 };
 
 export default IconButton
--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -5,7 +5,7 @@ import Navigator from '../elements/Navigator'
 const Sidebar = ({ children }: { children: ReactNode }) => {
   return (
     <section className="flex flex-row h-full">
-      <nav className="hidden lg:block w-60 border-r border-neutral-600">
+      <nav className="hidden lg:block w-60 border-r">
         <h1><Logo className="p-6" /></h1>
         <Navigator className="hidden" />
       </nav>
