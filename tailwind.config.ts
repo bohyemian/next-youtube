26d86e0837207733d24d820a5c53dea1c135f331
TOMBOY <bohyemian.kr@gmail.com>
2025-08-11 23:22:14 +0900

    break point 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,12 @@ const config: Config = {
   darkMode: ['class'],
   content: ['./pages/**/*.{js,ts,jsx,tsx,mdx}', './components/**/*.{js,ts,jsx,tsx,mdx}', './app/**/*.{js,ts,jsx,tsx,mdx}'],
   theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
