1b2a62ea86abd60e7b3f66411100f8ad7011c9e3
TOMBOY <bohyemian.kr@gmail.com>
2025-08-13 22:31:24 +0900

    태그 수정

--- components/elements/IconButton.tsx
+++ components/elements/IconButton.tsx
@@ -1,7 +1,7 @@
-import { ReactNode } from 'react';
+import { ReactNode, ComponentProps } from 'react';
 
-const IconButton = ({ icon, className, onIconClick }: { icon: ReactNode; className?: string; onIconClick?: () => void }) => {
-  return <div className={className} onClick={onIconClick}>{icon}</div>
+const IconButton = ({ type, icon, className, onIconClick }: ComponentProps<"button"> & { icon: ReactNode; className?: string; onIconClick?: () => void }) => {
+  return <button type={type || "button"} className={className} onClick={onIconClick}>{icon}</button>
 };
 
 export default IconButton
