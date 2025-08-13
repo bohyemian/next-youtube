2cb6de91940896860d25d74ee077bb742be2d840
TOMBOY <bohyemian.kr@gmail.com>
2025-08-13 22:41:43 +0900

    sidebar flex-shrink 속성 추가

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -5,7 +5,7 @@ import Navigator from '../elements/Navigator'
 const Sidebar = ({ children }: { children: ReactNode }) => {
   return (
     <section className="flex flex-row h-full">
-      <nav className="hidden lg:block overflow-y-auto w-60 border-r">
+      <nav className="hidden lg:block overflow-y-auto w-60 border-r flex-shrink-0">
         <h1><Logo className="p-6" /></h1>
         <Navigator className="hidden" />
       </nav>
