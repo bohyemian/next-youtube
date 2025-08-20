115fb2febfa4f39d61db33be2015f2b40beee357
TOMBOY <bohyemian.kr@gmail.com>
2025-08-20 18:24:38 +0900

    Chore: prettier 설치

---
+++ prettier.config.mjs
@@ -0,0 +1,15 @@
+export default {
+  arrowParens: 'always',
+  htmlWhitespaceSensitivity: 'css',
+  bracketSameLine: false,
+  bracketSpacing: true,
+  printWidth: 80,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+  endOfLine: 'lf',
+};
