899edfcd5a9f41ffd458669e9debeeaecd8fb593
TOMBOY <bohyemian.kr@gmail.com>
2025-08-20 20:20:32 +0900

    배열 셔플 util 추가

--- lib/utils.ts
+++ lib/utils.ts
@@ -33,3 +33,15 @@ export const chunkArray = <T>(arr: T[], chunkSize: number = 1) => {
 export const generateRandomHex = () => {
   return '#' + ((Math.random() * 0xffffff) << 0).toString(16).padStart(6, '0');
 };
+
+export const shuffleArray = <T>(array: T[]): T[] => {
+  const result = [...array];
+
+  for (let i = result.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+
+    [result[i], result[j]] = [result[j], result[i]];
+  }
+
+  return result;
+};
