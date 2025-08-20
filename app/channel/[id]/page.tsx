816119279b1d4daf4b54fdbb6aea18d3dc72ff95
TOMBOY <bohyemian.kr@gmail.com>
2025-08-20 20:20:17 +0900

    feat: 셔플 기능 추가

--- app/channel/[id]/page.tsx
+++ app/channel/[id]/page.tsx
@@ -1,12 +1,11 @@
-import { Button } from "@/components/ui/button";
 import { HeaderBgChanger } from "@/components/HeaderBgChanger";
 import { getChannelById } from "@/lib/dummyData";
 import { getRandomElementFromArray } from "@/lib/utils";
-import { FiRadio } from "react-icons/fi";
-import { TiArrowShuffle } from "react-icons/ti";
 import { permanentRedirect } from "next/navigation";
 import SongCard from "@/components/SongCard";
 import PlayListCarousel from "@/components/PlayListCarousel";
+import usePlayerStore from "@/store/usePlayerStore";
+import ChannelHead from "../components/ChannelHead";
 
 type ParamsProps = Promise<{ id: string[] }>;
 
@@ -23,11 +22,7 @@ const page = async (props: { params: ParamsProps }) => {
     <section className="contents-padding">
       <HeaderBgChanger imgSrc={imageSrc} />
       <h2 className="pt-20 text-2xl font-bold">{channel.name}</h2>
-      <div className="flex flex-row gap-3 flex-wrap mt-3 lg:max-w-[400px]">
-        <Button variant="outline" size="sm" className="px-5 border-neutral-400 bg-transparent text-sm rounded-full lg:w-full lg:flex-grow-0">구독중</Button>
-        <Button className="rounded-full flex-grow-0 px-5 text-sm lg:flex-grow" size="sm"><TiArrowShuffle size={12} />셔플</Button>
-        <Button className="rounded-full flex-grow-0 px-5 text-sm lg:flex-grow" size="sm"><FiRadio size={12} />뮤직 스테이션</Button>
-      </div>
+      <ChannelHead songList={channel.songList} />
       <h3 className="mt-12 text-xl font-bold">노래</h3>
       {channel.songList.map(song => <SongCard key={song.name} song={song} />)}
       <h3 className="mt-12 text-xl font-bold">앨범</h3>
---
+++ app/channel/components/ChannelHead.tsx
@@ -0,0 +1,23 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { shuffleArray } from "@/lib/utils";
+import { FiRadio } from "react-icons/fi";
+import { TiArrowShuffle } from "react-icons/ti";
+import usePlayerStore from "@/store/usePlayerStore";
+import type { Song } from "@/types";
+
+const ChannelHead = ({ songList }: { songList: Song[] }) => {
+  const addSongList = usePlayerStore(s => s.addSongList);
+  const handleShuffle = () => addSongList(shuffleArray(songList));
+
+  return (
+    <div className="flex flex-row gap-3 flex-wrap mt-3 lg:max-w-[400px]">
+      <Button variant="outline" size="sm" className="px-5 border-neutral-400 bg-transparent text-sm rounded-full lg:w-full lg:flex-grow-0">구독중</Button>
+      <Button className="rounded-full flex-grow-0 px-5 text-sm lg:flex-grow" size="sm" onClick={handleShuffle}><TiArrowShuffle size={12} />셔플</Button>
+      <Button className="rounded-full flex-grow-0 px-5 text-sm lg:flex-grow" size="sm"><FiRadio size={12} />뮤직 스테이션</Button>
+    </div>
+  )
+}
+
+export default ChannelHead
